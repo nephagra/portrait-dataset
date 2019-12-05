7231951d148122142046f370937a256c10e5d494
nephagra <nephele1997@gmail.com>
2019-12-05 16:03:35 -0500

    updated reconstruction app

--- js/source.js
+++ js/source.js
@@ -34,4 +34,32 @@ $(document).ready(function(){
         }
     })
 
+    
+    $('#randomz').on({
+        'click': function(){
+           $('#z1').val(Math.round(Math.random()*10)/10)
+           $('#z2').val(Math.round(Math.random()*10)/10)
+           $('#z3').val(Math.round(Math.random()*10)/10)
+           $('#z4').val(Math.round(Math.random()*10)/10)
+           $('#z5').val(Math.round(Math.random()*10)/10)
+           $('#z6').val(Math.round(Math.random()*10)/10)
+           $('#z7').val(Math.round(Math.random()*10)/10)
+           $('#z8').val(Math.round(Math.random()*10)/10)
+           $('#z9').val(Math.round(Math.random()*10)/10)
+           $('#z10').val(Math.round(Math.random()*10)/10)
+           $('#z11').val(Math.round(Math.random()*10)/10)
+           $('#z12').val(Math.round(Math.random()*10)/10)
+           $('#z13').val(Math.round(Math.random()*10)/10)
+           $('#z14').val(Math.round(Math.random()*10)/10)
+           $('#z15').val(Math.round(Math.random()*10)/10)
+        }
+    })
+
+    $('#loadportrait').on({
+        'click': function(){
+            i += 1
+            $('#reconin').attr('src','Images/' + metadata.responseJSON[i]['filename']);
+        }
+    })
+
 });
